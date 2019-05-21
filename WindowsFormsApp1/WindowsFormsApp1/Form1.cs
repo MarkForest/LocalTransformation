f293428faaeb35d170a16461373d05e9a67cd3fe
markforest <toxerius7@gmail.com>
2019-05-21 19:14:27 +0300

    commit1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Paint += Form1_Paint;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            using (Graphics graphics = e.Graphics)
+            {
+                //Создадим путь
+                GraphicsPath path = new GraphicsPath();
+                path.AddLine(20, 20, 200, 50);
+                path.AddArc(50, 50, 100, 50, 10, -220);
+
+                Matrix matrix = new Matrix();
+                //применяем вращение 
+                matrix.Rotate(40);
+                //применяем преобразования 
+                path.Transform(matrix);
+
+
+                //Визуализируем путь
+                graphics.DrawPath(new Pen(Color.Green, 6), path);
+                //освобождение ресурса
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/image_transform/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace image_transform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            using (Graphics graphics = e.Graphics)
+            {
+                try
+                {
+                    graphics.DrawImage(Image.FromFile("1.jpg"), 0, 0, 100, 100);
+                }
+                catch  {}
+                Matrix matrix = new Matrix();
+                matrix.RotateAt(45, new PointF(150, 150));
+                matrix.Translate(100, 100);
+                graphics.Transform = matrix;
+
+                try
+                {
+                    graphics.DrawImage(Image.FromFile("2.jpg"), 0, 0, 100, 100);
+                }
+                catch{}
+
+                matrix.Reset();
+                matrix.Translate(150, 10);
+                matrix.Shear(0.5f, 0.3f);
+
+                graphics.Transform = matrix;
+                try
+                {
+                    graphics.DrawImage(Image.FromFile("2.jpg"), 0, 0, 100, 100);
+                }
+                catch { }
+            }
+        }
+    }
+}
